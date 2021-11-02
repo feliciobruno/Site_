02ccc6fa21d60b80db5e8ef08ec41af8836d5cfd
Bruno Felicio <brunofelicio.pnl@gmail.com>
2021-11-02 19:14:34 -0300

    script.js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+//javascript é interatividade
+
+var nome = window.prompt('Olá, como você se chama?')  //abrirá um prompt para o usuario escrever algo.
+var idade = window.prompt('Qual a sua idade?')
+window.alert(`Seja bem vindo(a), ${nome}! Vejo que você tem ${idade} anos`)
+window.confirm('Vamos começar nossa jornada?') //como resposta: OK ou CANCELA.
+
+
